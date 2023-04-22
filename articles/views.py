d3b5db8c7a77183cb525f5a9f910fcfae581bf56
Yi Yoonsung <lys11556633@gmail.com>
2023-04-22 16:55:54 +0900

    Test1 serial

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from articles.models import Article
+from articles.serializers import ArticleSerializer
 
 
 # Create your views here.
 @api_view(['GET', 'POST'])
 def index(request):
-    return Response("연결되었습니다!")
+    articles = Article.objects.all()
+    article = articles[0]
+    serializer = ArticleSerializer(article)
+    return Response(serializer)
