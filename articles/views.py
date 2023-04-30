582f4d69af67ef1b7d57ea44f6354644f73b361d
Yi Yoonsung <lys11556633@gmail.com>
2023-05-01 03:14:37 +0900

    :art:Style:게시글 리스트 작성 & 포스트맨 테스트

--- articles/views.py
+++ articles/views.py
@@ -1,16 +1,24 @@
 from rest_framework.views import APIView
 from rest_framework import status, permissions
 from rest_framework.response import Response
+from articles.models import Article
+from articles.serializers import ArticleSerializer, ArticleListSerializer, ArticleCreateSerializer
 
 
 
 class ArticleView(APIView):
     def get(self, request):
-        
-        pass
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
-        pass
+        serializer = ArticleCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
     
 
 class ArticleDetailView(APIView):
